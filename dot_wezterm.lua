8665561b0a57c7f62a882622bf5c672ae39dbc12
spicyDogWings <spaceprotondev@proton.me>
2025-11-03 22:32:16 -0600

    nov 3 -> 3

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -10,10 +10,31 @@ config.font = wezterm.font_with_fallback {
   'JetBrains Mono',
   'Hack',
 }
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.9
 
 -- Configuración de tabs
-config.enable_tab_bar = true
+config.enable_tab_bar = false
 config.tab_bar_at_bottom = true
+
+config.window_frame = {
+  active_titlebar_bg = '#020203',
+  inactive_titlebar_bg = '#121213',
+}
+
+config.colors = {
+  tab_bar = {
+    background = '#0b0022',
+    active_tab = {
+      bg_color = '#020203',
+      fg_color = '#6886A4',
+      intensity = 'Normal',
+    },
+    inactive_tab = {
+      bg_color = '#020203',
+      fg_color = '#323233',
+    },
+  }
+}
+
 
 return config
